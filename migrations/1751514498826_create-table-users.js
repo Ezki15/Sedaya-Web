dff8299c71130d24eddeb8d4483088159321343b
Ezki15 <110952841+Ezki15@users.noreply.github.com>
2025-07-03 12:13:43 +0800

    Create dabase and table

---
+++ migrations/1751514498826_create-table-users.js
@@ -0,0 +1,45 @@
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+  pgm.createTable('users', {
+    id: {
+      type: 'VARCHAR(20)',
+      primaryKey: true,
+    },
+    fullname: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    username: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+      unique: true,
+    },
+    email: {
+      type: 'VARCHAR(100)',
+      notNull: true,
+      unique: true,
+    },
+    password: {
+      type: 'VARCHAR(255)',
+      notNull: true,
+    },
+    created_at: {
+      type: 'TIMESTAMP',
+      notNull: true,
+      default: pgm.func('current_timestamp'),
+    },
+  });
+};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+  pgm.dropTable('users');
+};
