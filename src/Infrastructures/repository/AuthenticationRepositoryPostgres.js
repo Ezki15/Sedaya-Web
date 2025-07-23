b5fbaeb4d5ae24487023b16823faeac65efc0d7a
Ezki15 <110952841+Ezki15@users.noreply.github.com>
2025-07-23 22:52:21 +0800

    Infrastructures update: AuthenticationRepositoryPostgres implemnted

---
+++ src/Infrastructures/repository/AuthenticationRepositoryPostgres.js
@@ -0,0 +1,40 @@
+import InvariantError from '../../Commons/exceptions/InvariantError';
+import AuthenticationRepository from '../../Domains/authentications/AuthenticationRepository';
+
+class AuthenticationRepositoryPostgres extends AuthenticationRepository {
+  constructor(pool) {
+    super();
+    this._pool = pool;
+  }
+
+  async addToken(token) {
+    const query = {
+      text: 'INSERT INTO authentications VALUES($1)',
+      values: [token],
+    };
+
+    await this._pool.query(query);
+  }
+
+  async checkAvailabilityToken(token) {
+    const query = {
+      text: 'SELECT token FROM authentications WHERE token = $1',
+      values: [token],
+    };
+    const result = await this._pool.query(query);
+    if (!result.rows.length) {
+      throw new InvariantError('Token tidak tersedia');
+    }
+  }
+
+  async deleteToken(token) {
+    const query = {
+      text: 'DELETE FROM authentications WHERE token = $1',
+      values: [token],
+    };
+
+    await this._pool.query(query);
+  }
+}
+
+export default AuthenticationRepositoryPostgres;
---
+++ test/AuthenticationsTableTestHelper.js
@@ -0,0 +1,35 @@
+/* istanbul ignore file */
+
+import pool from '../src/Infrastructures/database/postgres/pool';
+
+const AuthenticationsTableTestHelper = {
+  async addToken(token) {
+    const query = {
+      text: 'INSERT INTO authentications VALUES($1)',
+      values: [token],
+    };
+
+    await pool.query(query);
+  },
+
+  async findToken(token) {
+    const query = {
+      text: 'SELECT token FROM authentications WHERE token = $1',
+      values: [token],
+    };
+
+    const result = await pool.query(query);
+
+    return result.rows;
+  },
+
+  async cleanTable() {
+    const query = {
+      text: 'DELETE FROM authentications',
+    };
+
+    await pool.query(query);
+  },
+};
+
+export default AuthenticationsTableTestHelper;
